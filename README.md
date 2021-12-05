aca59d0871b552c928c6bfe30634d6be491f5eee
Ida Delphine <idadelm@gmail.com>
2021-12-05 23:32:13 +0100

    Create starter project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Web3 Confessions 👋 
+
+### **Welcome!**
+To get started, clone this repo and follow these commands:
+
+1. Run `npm install` at the root of your directory
+2. Run `npm run start` to start the project
+
+
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { ethers } from "ethers";
+import './App.css';
+
+export default function App() {
+
+  const confess = () => {
+    
+  }
+  
+  return (
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+        😈 Confess!!!
+        </div>
+
+        <div className="bio">
+        I'm Ida and I want to know something about you that nobody knows. Kinda a secret. Good or Evil. It could be something you've done or something you plan on doing or something you think about someone. A secret you'll take to your grave 😉
+        </div>
+
+        <button className="confessButton" onClick={confess}>
+        I want to confess
+        </button>
+      </div>
+    </div>
+  );
+}
